7732cffbf4b4df666ca62df27b99500ab28fe741
João Victor <joaovictormdasilva676@gmail.com>
2024-08-31 22:18:59 -0300

    :bug: fix: menu hamburger

--- grupo_4/src/Componentes/Header/MenuHamburger/index.jsx
+++ grupo_4/src/Componentes/Header/MenuHamburger/index.jsx
@@ -2,10 +2,12 @@ import styles from './MenuHamburger.module.css';
 
 export default function MenuHamburger({ exibirMenu }) {
     return (
-        <div className={styles.menu_hamburge__div} onClick={exibirMenu}>
-            <span className={styles.hamburge}></span>
-            <span className={styles.hamburge}></span>
-            <span className={styles.hamburge}></span>
+        <div className={styles.background} onClick={exibirMenu}>
+            <button className={styles.menu__icon}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
         </div>
     );
 };
