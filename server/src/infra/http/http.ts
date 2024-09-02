b9a2761b69b4aa5ee55cc85e422e350e91053090
Fabriziosilveira <silveira6602@gmail.com>
2024-09-02 16:38:14 -0300

    :sparkles: feat: Create Server entri point

---
+++ server/src/infra/http/http.ts
@@ -0,0 +1,3 @@
+export interface HTTP{
+    start(port: number): void;
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
-import express from "express";
+import { CreateProductUseCase } from "./application/useCase/product/Create-Product.useCase";
+import { prisma } from "./infra/database/prisma";
+import 'dotenv/config'
+import { CreateProductRoute } from "./infra/http/express/routes/products/Create-Product.routes";
+import { ProductRepositoryPrisma } from "./infra/repositories/product/Product.repository.prisma";
+import { ApiExpress } from "./infra/http/express/api.express";
 
-const app = express();
-const PORT = 3000;
+function main() {
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${PORT}.`);
-});
+    const repository = ProductRepositoryPrisma.create(prisma);
+
+    const createProductUseCase = CreateProductUseCase.create(repository);
+    // definir o getall
+
+    const createRoute = CreateProductRoute.create(createProductUseCase);
+    // definir routas do getAll
+    
+    const port = process.env.PORT as string;
+
+    const api = ApiExpress.create(
+        [
+            createRoute
+        ]
+    );
+    api.start(parseInt(port));
+}
+
+main();
